135f049d160a9722caeed6a162b659af500ac372
Georgios Charmanis <georgios.charmanis@nttdata.com>
2024-02-27 14:47:44 +0200

    Initial commit

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+  shared_credentials_files = ["/home/donquixote/.aws/credentials"]
+}
+
+
+resource "aws_instance" "jocker" {
+  ami           = "ami-0c7217cdde317cfec" #Ubuntu server 22.04 
+  instance_type = "t2.micro"
+}
+
+
