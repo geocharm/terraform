431b7296b1bffd7491dd59b53d132527e66f9c21
Georgios Charmanis <georgios.charmanis@nttdata.com>
2024-02-28 15:37:27 +0200

    VPC - Subnet

--- main.tf
+++ main.tf
@@ -11,10 +11,28 @@ provider "aws" {
   shared_credentials_files = ["/home/donquixote/.aws/credentials"]
 }
 
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+resource "aws_subnet" "subnet-1" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "prod-subnet"
+  }
+}
 
 resource "aws_instance" "jocker" {
   ami           = "ami-0c7217cdde317cfec" #Ubuntu server 22.04 
   instance_type = "t2.micro"
 }
+
+
 
 
